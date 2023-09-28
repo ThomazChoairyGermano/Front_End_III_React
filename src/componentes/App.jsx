fda3c4872a53a5e7c34bd5bc6241b4e2996622ef
ThomazChoairyGermano <104391398+ThomazChoairyGermano@users.noreply.github.com>
2023-09-27 21:10:27 -0300

    novo2

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import Primeiro from "./basico/Primeiro"
+import Segundo from "./basico/Segundo"
 
 export default function App(){
     return(
-        Primeiro()
+        <div>
+            <Primeiro/>
+            <Segundo titulo1= 'titulo1' titulo2 = 'titulo2'/> 
+        </div>
 )
 
 }
